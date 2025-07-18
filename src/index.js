246481151e8db64a328f8049bbe56cdefbaf9380
Kartik Nath Khatri <masterbro770@gmail.com>
2025-07-18 21:18:19 +0530

    updating code till utilites of API error and Api Response

---
+++ src/index.js
@@ -0,0 +1,67 @@
+import dotenv from 'dotenv';
+import connectDB from "./db/DB.js"
+// import express from 'express';
+
+
+dotenv.config({
+    path: './env'
+})
+
+/* *****************with importing DB (RECOMMENDED) *********** */
+
+// Async function give promises that why we are able to use .then and catch
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log(`Server is running on Port : ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB connection failed ",error);
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+/*  ************************************** without importing ************** */
+
+/*
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+const app =express();
+(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error",(error)=>{
+            console.log("ERROR:",error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+            
+        })
+    } catch (error) {
+        console.log("ERROR :",error);
+        
+    }
+})() */
+
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,34 @@
+const asyncHandler=(fn)=>{
+    async (req,res,next)=>{
+        try {
+            await fn(req,res,next);
+        } catch (error) {
+            res.status(error.code || 500).json({
+                success: false,
+                message: error.message
+            })
+        }
+    }
+}
+
+
+// ********** into promises ***********
+
+/*
+const asyncHandler = (fn) => {
+    return (req, res, next) => {
+        Promise.resolve(fn(req, res, next))
+            .catch((error) => {
+                res.status(error.code || 500).json({
+                    success: false,
+                    message: error.message,
+                });
+            });
+    };
+};
+*/
+
+
+
+
+export {asyncHandler};
