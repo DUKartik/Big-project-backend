4134880d415c82246de6db597213a919f127d899
Kartik Nath Khatri <masterbro770@gmail.com>
2025-07-21 12:51:20 +0530

    fix async handler

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,15 +1,13 @@
-const asyncHandler=(fn)=>{
-    async (req,res,next)=>{
+const asyncHandler = (fn) => {
+    return async (req, res, next) => {
         try {
-            await fn(req,res,next);
+            await fn(req, res, next);
         } catch (error) {
-            res.status(error.code || 500).json({
-                success: false,
-                message: error.message
-            })
+            next(error);
         }
-    }
-}
+    };
+};
+
 
 
 // ********** into promises ***********
@@ -19,10 +17,7 @@ const asyncHandler = (fn) => {
     return (req, res, next) => {
         Promise.resolve(fn(req, res, next))
             .catch((error) => {
-                res.status(error.code || 500).json({
-                    success: false,
-                    message: error.message,
-                });
+                next(error)
             });
     };
 };
