ac86b913586c758e677b2f1644570a09e51312cf
Kartik Nath Khatri <masterbro770@gmail.com>
2025-09-01 17:48:53 +0530

    subscription controllers updated

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,4 +1,5 @@
 import { Subscription } from "../models/subscription.model.js";
+import { User } from "../models/user.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
@@ -6,19 +7,66 @@ import { ApiError } from "../utils/ApiError.js";
 // firstly run auth middleware to chck whether the user is logined or not
 // now get subscriber from req.user
 //and channel from req.prams
-const subscribeChannel = asyncHandler( async(req,res)=>{
-    const subscriber =req.user;
-    const subscription = await Subscription.create({
-        channel,
-        subscriber
-    })
+const toggleSubscription = asyncHandler(async (req,res)=>{
+    const {channelId} =req.params;
+    const user = req.user;
+    
+    const channel = await User.findById(channelId);
+    if(!channel){
+        throw new ApiError(404,"Channel doesn't exist");
+    }
+    const Subscribed = await Subscription.findOne({channel:channel,subscriber:user});
+    
+    if(!Subscribed){
+        const subscription =await Subscription.create({
+            subscriber:user._id,
+            channel: channel._id
+        })
+        return res
+        .status(200)
+        .json(
+           new ApiResponse(200,subscription,"Channel Subscribed Successfully")
+        )
+    }
+    const deleted=await Subscription.deleteOne({_id:Subscribed._id});
+    return res
+    .status(200)
+    .json(
+       new ApiResponse(200,deleted,"Unsubscribed Channel Successfully")
+    )
+})
+
+const getUserChannelSubscribers = asyncHandler(async (req,res)=>{
+    const {channelId} = req.params;
+    const channel = await User.findById(channelId);
+    if(!channel){
+        throw new ApiError(404,"Channel Not found");
+    }
+    const subscriber = await Subscription.find({channel:channel._id}).select({subscriber:1});
 
     return res
     .status(200)
     .json(
-        new ApiResponse(200,subscription,"subscribed channel successfully"))
+        new ApiResponse(200,subscriber,"Subscriber list fetched Successfully")
+    )
 })
 
+const getSubscribedChannels = asyncHandler(async(req,res)=>{
+    const {subscriberId} =req.params;
+    const user = await User.findById(subscriberId);
+    if(!user){
+        throw new ApiError(404,"User not found");
+    }
+    const channels = await Subscription.find({subscriber:user._id}).select({channel:1});
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,channels,"channels data successfully fetched")
+    )
+})
 export{
-    subscribeChannel
+    toggleSubscription,
+    getSubscribedChannels,
+    getUserChannelSubscribers
 }
