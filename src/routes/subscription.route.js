28ee1dcfd289b788373c00c1b38ef270792736a6
Kartik Nath Khatri <masterbro770@gmail.com>
2025-09-02 14:51:08 +0530

    subscription routes defined

---
+++ src/routes/subscription.route.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import {
+    toggleSubscription,
+    getSubscribedChannels,
+    getUserChannelSubscribers
+} from "../controllers/subscription.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router =Router();
+
+router.route("/toggle/:channelId").get(verifyJWT,toggleSubscription);
+router.route("/Channel/:subscriberId").get(verifyJWT,getSubscribedChannels);
+router.route("/Subscriber/:channelId").get(verifyJWT,getUserChannelSubscribers);
+
+export default router;
